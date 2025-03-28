f65e615c1e96e2da202b8991eeccb884d42fd77e
jsonsvalue <hellojaeseok@naver.com>
2025-03-28 16:12:41 +0900

    Prim & Dijkstra Algorithm upload

--- JavaAPS/APS응용_Day09_그래프최소비용2/src/그래프최소비용_다익스트라_배열.java
+++ JavaAPS/APS응용_Day09_그래프최소비용2/src/그래프최소비용_다익스트라_배열.java
@@ -54,11 +54,14 @@ public class 그래프최소비용_다익스트라_배열 {
 		boolean[] visited = new boolean[V];
 		dist[start] = 0; // 시작 거리의 값을 0으로 갱신한다.
 		
+		
 		for(int i=0; i < V; i++) {
 			int min = INF;
 			int idx = -1;
 			
-			for(int j=0; j<V; j++) {
+			// 1. 현재 정점과 연결된 정점들과의 최소 거리를, dist 배열에 저장한다.
+			// 현재 정점과 연결된 정점 중 최소 거리를 가진 정점을 확인한다.
+			for(int j = 0; j < V; j++) {
 				if(!visited[j] && dist[j] < min) {
 					min = dist[j];
 					idx = j;
@@ -67,10 +70,11 @@ public class 그래프최소비용_다익스트라_배열 {
 			
 			visited[idx] = true;
 			
-			// 2. 갱신 작업
+			// 2. 최소 거리 갱신 작업
+			// 현재 정점과 연결된 최소 거리를 가진 정점에서, 해당 정점과 연결된 노드를 통해서 갈 수 있는 다른 노드와의 거리를 찾고 최소 거리를 갱신한다.
 			for(Edge e: adj[idx]) {
-				// 나와 인접한 노드 중 방문하지 않았고, 갱신할 수 있는 노드를 확인한다.
-				if(!visited[e.to] && dist[e.to] > dist[idx]+e.cost) {
+				// 현재 노드와 인접한 노드 중 방문하지 않았고, 특정 노드에서 도착 노드까지의 거리보다 다른 노드를 거쳤을 때의 거리가 더 짧으면 거리를 갱신한다.
+				if(!visited[e.to] && dist[e.to] > dist[idx] + e.cost) {
 					dist[e.to] = dist[idx] + e.cost;
 				}
 			}
